940000b506e0f693b1daf95b730371d66a255fbc
Dan Grebb <git@dgrebb.com>
2024-07-27 03:07:31 -0400

    fix(tests): fix tests (and action)

--- actions/hello-world/src/main.js
+++ actions/hello-world/src/main.js
@@ -1,15 +1,30 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
+const { wait } = require('./wait')
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet')
-  console.log(`Hello ${nameToGreet}!`)
-  const time = new Date().toTimeString()
-  core.setOutput('time', time)
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
-} catch (error) {
-  core.setFailed(error.message)
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+async function run() {
+  try {
+    const ms = core.getInput('milliseconds', { required: true })
+
+    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
+    core.debug(`Waiting ${ms} milliseconds ...`)
+
+    // Log the current timestamp, wait, then log the new timestamp
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
+
+    // Set outputs for other workflow steps to use
+    core.setOutput('time', new Date().toTimeString())
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    core.setFailed(error.message)
+  }
+}
+
+module.exports = {
+  run
 }
