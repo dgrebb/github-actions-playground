c082c9d1ae7feedca28f5945b765f92f93574bd8
Dan Grebb <git@dgrebb.com>
2024-07-27 10:39:43 -0400

    wip(jira): configure release name from inputs

--- actions/create-jira-release/src/main.js
+++ actions/create-jira-release/src/main.js
@@ -18,6 +18,9 @@ async function run() {
     const JIRA_RELEASE_IDENTIFIER = core.getInput('JIRA_RELEASE_IDENTIFIER', {
       required: true
     })
+    const JIRA_RELEASE_DESCRIPTION = core.getInput('JIRA_RELEASE_DESCRIPTION', {
+      required: true
+    })
     const JIRA_RELEASE_NOW = core.getInput('JIRA_RELEASE_NOW', {
       required: true
     })
@@ -32,7 +35,7 @@ async function run() {
           Accept: 'application/json'
         }
       }
-    ).data.id
+    )
 
     core.summary.addRaw('# Initial Vars')
 
@@ -41,17 +44,22 @@ async function run() {
       `JIRA_API_URL: ${JIRA_API_URL}`,
       `JIRA_PROJECT_KEY: ${JIRA_PROJECT_KEY}`,
       `JIRA_RELEASE_IDENTIFIER: ${JIRA_RELEASE_IDENTIFIER}`,
-      `JIRA_PROJECT_ID: ${JIRA_PROJECT_ID}`
+      `JIRA_PROJECT_ID: ${JIRA_PROJECT_ID}`,
+      `JIRA_RELEASE_NOW: ${JIRA_RELEASE_NOW}`
     ])
 
     core.debug('--- VARIABLES ---')
     core.debug(`JIRA_URL: ${JIRA_URL}`)
     core.debug(`JIRA_API_URL: ${JIRA_API_URL}`)
     core.debug(`JIRA_PROJECT_KEY: ${JIRA_PROJECT_KEY}`)
-    core.debug(`JIRA_RELEASE_IDENTIFIER: ${JIRA_RELEASE_IDENTIFIER}`)
     core.debug(`JIRA_PROJECT_ID: ${JIRA_PROJECT_ID}`)
+    core.debug(`JIRA_RELEASE_IDENTIFIER: ${JIRA_RELEASE_IDENTIFIER}`)
+    core.debug(`JIRA_RELEASE_DESCRIPTION: ${JIRA_RELEASE_DESCRIPTION}`)
+    core.debug(`JIRA_RELEASE_NOW: ${JIRA_RELEASE_NOW}`)
 
     core.debug(`Create Jira Fix Version(s) ...`)
+
+    const JIRA_RELEASE_NAME = `${JIRA_PROJECT_KEY}-R${JIRA_RELEASE_IDENTIFIER}`
 
     const JIRA_VERSION_ID = await fetch(`${JIRA_API_URL}/version`, {
       method: 'GET',
@@ -62,25 +70,24 @@ async function run() {
       },
       body: JSON.stringify({
         archived: false,
-        description: '${{ env.JIRA_RELEASE_DESCRIPTION }}',
-        name: '${{ env.JIRA_VERSION_NAME }}',
+        description: `${JIRA_RELEASE_DESCRIPTION}`,
+        name: `${JIRA_RELEASE_NAME}`,
         projectId: `${JIRA_PROJECT_ID}`,
         released: `${JIRA_RELEASE_NOW} || 'false' }}`
       })
-    }).data.id
+    })
 
     const VERSION_URL = `${JIRA_URL}/projects/${JIRA_PROJECT_KEY}/versions/${JIRA_VERSION_ID}`
-    const VERSION_NAME = `${JIRA_PROJECT_KEY}-R${JIRA_RELEASE_IDENTIFIER}`
 
     core.summary.addRaw('# Generated Vars')
 
     core.summary.addList([
       `VERSION_URL: ${VERSION_URL}`,
-      `VERSION_NAME: ${VERSION_NAME}`
+      `JIRA_RELEASE_NAME: ${JIRA_RELEASE_NAME}`
     ])
 
     // Set outputs for other workflow steps to use
-    core.setOutput('JIRA_VERSION_NAME', VERSION_NAME)
+    core.setOutput('JIRA_RELEASE_NAME', JIRA_RELEASE_NAME)
     core.setOutput('JIRA_VERSION_URL', VERSION_URL)
   } catch (error) {
     // Fail the workflow run if an error occurs
