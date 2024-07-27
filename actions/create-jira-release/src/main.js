618706843b75786ab92f13ce9b758409fffde286
Dan Grebb <git@dgrebb.com>
2024-07-27 17:20:25 -0400

    docs(actions): add more error handling and useful links to summary

--- actions/create-jira-release/src/main.js
+++ actions/create-jira-release/src/main.js
@@ -48,11 +48,12 @@ async function run() {
 
   const headers = createJiraHeaders(JIRA_API_USER, JIRA_API_TOKEN)
   let JIRA_PROJECT_NAME
+  let JIRA_RELEASE_IDENTIFIER
   let JIRA_RELEASE_NAME
   let suggestedIdentifier
 
   try {
-    const JIRA_RELEASE_IDENTIFIER = core.getInput('JIRA_RELEASE_IDENTIFIER', {
+    JIRA_RELEASE_IDENTIFIER = core.getInput('JIRA_RELEASE_IDENTIFIER', {
       required: true
     })
     const JIRA_RELEASE_DESCRIPTION = core.getInput('JIRA_RELEASE_DESCRIPTION', {
@@ -96,9 +97,9 @@ async function run() {
         const JIRA_RELEASE_ID = jiraResponse.id
         const JIRA_RELEASE_URL = `${JIRA_URL}/projects/${JIRA_PROJECT_KEY}/versions/${JIRA_RELEASE_ID}`
 
-        core.summary.addRaw(`## Release Details for ${JIRA_PROJECT_NAME}`, true)
+        core.summary.addRaw(`### ✅ ${JIRA_PROJECT_NAME} Release Created`, true)
         core.summary.addRaw(
-          `- Release: [${JIRA_RELEASE_NAME}](${JIRA_RELEASE_URL})`,
+          `- Jira Version: [${JIRA_RELEASE_NAME}](${JIRA_RELEASE_URL})`,
           true
         )
         core.summary.addRaw(
@@ -114,27 +115,58 @@ async function run() {
           // Suggest a new identifier if there's a conflict
           suggestedIdentifier = incrementIdentifier(JIRA_RELEASE_IDENTIFIER)
           core.summary.addRaw(
-            `- <code>${JIRA_RELEASE_NAME}</code> is already present in Jira — check [${JIRA_PROJECT_NAME}](${JIRA_URL}/projects/${JIRA_PROJECT_KEY}). Does <code>${JIRA_PROJECT_KEY}-${suggestedIdentifier}</code> work for the team?`,
+            `- <code>${JIRA_RELEASE_NAME}</code> is already present in Jira — check [${JIRA_PROJECT_NAME}](${JIRA_URL}/projects/${JIRA_PROJECT_KEY})'s releases.`,
             true
           )
           core.setFailed(
             `${JIRA_PROJECT_NAME} already has a version named ${JIRA_RELEASE_NAME}. ` +
               `Suggested next version: ${JIRA_PROJECT_KEY}-${suggestedIdentifier}. Please update the workflow inputs and re-run.`
           )
+        } else if (postError.message === '401') {
+          core.summary.addRaw(`### 401`, true)
+          core.summary.addRaw(
+            `The request has invalid credentials, or no credentials when credentials are required, or the user doesn't have permissions to do that. Check Jira Project permissions for [${JIRA_PROJECT_KEY}](${JIRA_URL}/projects/${JIRA_PROJECT_KEY}).`,
+            true
+          )
+          core.setFailed(
+            `It looks like the Jira API user doesn't have access to [${JIRA_PROJECT_KEY}](${JIRA_URL}/projects/${JIRA_PROJECT_KEY}).`
+          )
+        } else if (postError.message === '404') {
+          core.summary.addRaw(`### 404`, true)
+          core.summary.addRaw(
+            `Not Found	We don't have a route registered there, e.g. it's not possible to GET /1/cards, or POST /1/elephants. Or the model the request is trying to operate on couldn't be found. Or some nested resource can't be found.`,
+            true
+          )
+          core.setFailed(
+            `The Jira API returned a 401 Unauthorized status. Do the included projects exist under the provided Jira Project Key: [${JIRA_PROJECT_KEY}](${JIRA_URL}/projects/${JIRA_PROJECT_KEY})?`
+          )
         } else {
           throw postError
         }
       }
     }
-
-    core.summary.addCodeBlock(`${suggestedIdentifier}`)
-
-    core.summary.write({ overwrite: false })
+    if (suggestedIdentifier) {
+      core.summary.addRaw(
+        `## ${JIRA_RELEASE_IDENTIFIER}'s name conflicts with a release in the above project(s).`,
+        true
+      )
+      core.summary.addRaw(
+        `Copy the next release iteration below ${suggestedIdentifier} and re-run the workflow.`,
+        true
+      )
+      core.summary.addCodeBlock(`${suggestedIdentifier}`)
+    } else {
+      core.summary.addRaw(
+        `## 🎈 ${JIRA_RELEASE_IDENTIFIER} created successfully.`,
+        true
+      )
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     console.error('Error message:', error.message)
     core.setFailed(`Error: ${error.message}`)
   }
+  core.summary.write({ overwrite: false })
 }
 
 module.exports = {
